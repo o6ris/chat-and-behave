6c6d07c86fb37bfa9ec6921e0ebc053225fb4d75
emmayylu <44856279+emmayylu@users.noreply.github.com>
2025-01-25 12:49:00 -0700

    ai frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    host: 'localhost',
+    port: 8080,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+    allowedHosts: 'all'
+  },
+};
